a88ed9d7abee248f55636eaf8fcb94d804440af1
Dave Denlow and Jesse McKay <dave+jesse@>
2016-11-29 14:06:12 -0800

    add a method to input two words, break them into an array, put them to lowercase, then push them into a list

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -12,11 +12,9 @@ namespace Anagram
         return View["index.cshtml"];
       };
       Post["/form"] = _ => {
-        var word1 = Request.Form["word1"];
-        var word2 = Request.Form["word2"];
-        AnagramGenerator newAnagramGenerator = new AnagramGenerator();
-        newAnagramGenerator.Compare(word1, word2);
-        return View["result.cshtml", newAnagramGenerator];
+        string wordOriginal = Request.Form["word-original"];
+        string wordAnagram = Request.Form["word-anagram"];
+        AnagramGenerator newAnagramGenerator = new AnagramGenerator(wordOriginal, wordAnagram);
       };
     }
   }
--- Objects/Anagram.cs
+++ Objects/Anagram.cs
@@ -5,7 +5,22 @@ namespace Anagram.Objects
 {
   public class AnagramGenerator
   {
-  
+
+    private string _wordOriginal;
+    private string _wordAnagram;
+    private List<string> _lettersList = new List<string>{};
+
+    public Anagram(string wordOriginal, string wordAnagram)
+    {
+      _wordOriginal = wordOriginal;
+      _wordAnagram = wordAnagram;
+      Char[] lettersArray = wordOriginal.ToLower().ToCharArray();
+      foreach (letter in lettersArray)
+      {
+        _lettersList.Add(letter.ToString());
+      }
+      _lettersList.Sort();
+    }
 
   }
 }
