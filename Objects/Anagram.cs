b51b86385a7aca6b883249c3d36d029c9629ee9e
Dave Denlow and Jesse McKay <dave+jesse@>
2016-11-29 16:31:27 -0800

    print sorted list onto index.html

--- Objects/Anagram.cs
+++ Objects/Anagram.cs
@@ -7,10 +7,10 @@ namespace Anagram.Objects
   public class AnagramGenerator
   {
 
-    private string _wordOriginal;
-    private List<string> _letters = new List<string>{};
+    private string _wordOriginal; // holds user input
+    private List<string> _letters = new List<string>{};  //holds letters after sorting
 
-    public AnagramGenerator(string word)
+    public AnagramGenerator(string word) // Constructor
     {
       _wordOriginal = word;
       Char[] charArray = word.ToUpper().ToCharArray();
@@ -19,6 +19,11 @@ namespace Anagram.Objects
         _letters.Add(letter.ToString());
       }
       _letters.Sort();
+    }
+    
+    public List<string> GetLettersList()
+    {
+      return _letters;
     }
   }
 }
